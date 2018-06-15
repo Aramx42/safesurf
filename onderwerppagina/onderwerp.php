d8ca4489ba0aeb27734dc7a85a00ab562dd4baaf
Aramx42 <40027585+Aramx42@users.noreply.github.com>
2018-06-15 12:41:31 +0200

    leerlingen
    page = startpagina
    onderwerp = schurkeneiland
    les = les 1

---
+++ onderwerppagina/onderwerp.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>Safesurf</title>
+</head>
+<body style="background-color: #33C3FF";
+>
+
+
+        <!-- nickname -->
+        <div style="position: absolute; left: 0%; top: 2%;">
+            <a class="text-box-text" align="center">Nickname </a>
+        </div>
+             
+   
+    
+         <!-- zoekfunctie  -->
+        <div style="position: absolute; left: 93.5%; top: 0%;">
+   		<input type="text" placeholder="Search.." style="color: black"> <!-- Fix dit-->
+        </div>
+       
+            <!-- go back icon linksonderin  -->
+            <div style="position: absolute; left: 2%; top: 90%;">
+                <a href="index.php">
+                    <img border="0" alt="W3Schools" src="images/go-back-icon.png" width="75" height="75">
+                </a>
+            </div>
+             
+			 <!-- stopknop linksonderin -->
+			 <div style="position: absolute; left: 8%; top: 90%;">
+                <a href="index.php">
+                    <img border="0" alt="W3Schools" src="images/stop.png" width="75" height="75">
+                </a>
+            </div>  
+    
+            <!--robot -->
+            <div style="position: absolute; left: 70%; top: 9%;">
+                <img src="images/robot.png" width="100" height="150"> 
+            </div>
+   
+    
+    
+    <!--Les elementen -->
+    <div class="div-onderwerp">
+	
+	<!--Personage -->
+		<div style="position: absolute; left: 35%; top: 30%;">
+             <img src="images/karakter.png" width ="250" height ="187"></a>
+        </div>
+    <!-- elementen-->
+        <div style="position: absolute; right: -10%; top: 10%;">
+            <a href="ettereiland.php"> <img src="images/150x150.png"></a>
+        </div>
+        
+        <div style="position: absolute; right: -10%; bottom: -10%;">
+            <a href="priveeiland.php"> <img src="images/150x150.png"></a>
+        </div>
+        
+
+        
+        <div style="position: absolute; left: -10%; bottom: -10%;" >
+            <a href="viruseiland.php"> <img src="images/150x150.png"></a>
+        </div>
+        
+        <div style="position: absolute; left: 35%; bottom: -10%;">
+            <a href="wijsheidseiland.php"> <img src="images/150x150.png"></a>
+        </div>
+		
+		<div style="position: absolute; left: 35%; top: -10%;">
+            <a href="wijsheidseiland.php"> <img src="images/150x150.png"></a>
+        </div>
+		
+		<div style="position: absolute; left: -10%; top: -10%;">
+            <a href="wijsheidseiland.php"> <img src="images/150x150.png"></a>
+        </div>
+		
+		<div style="position: absolute; left: 0%; top: -50%;" >
+             <img src="images/schurkeneiland.png" width="90%" height="100%"></a>
+		</div>
+		
+
+    
+    </div>
+
+</body>
+</html>
